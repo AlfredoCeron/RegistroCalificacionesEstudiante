d513be3a63e219f87079c462cf8fc6ad024c5d6c
AlfredoCeron <lalito45_@hotmail.com>
2026-06-06 00:45:03 -0600

    feat: Eliminar asignacion de X rubro

--- RegistroCalificaciones.py
+++ RegistroCalificaciones.py
@@ -7,6 +7,7 @@
 
 . Mostrar historial de calificaciones de X rubro
     .Borrar calif por indice/numero en la lista del historial de calificaciones
+    .Editar/Cambiar calir por indice/numero en la lista del historial de calificaciones
 """    
 
 def registrar_rubros():
@@ -37,25 +38,38 @@ def mostrar_calificaciones(rubros):
         print(f"\t{i+1}. {list(rubros.keys())[i]}") #Diccionario a lista para aprovechar el indice
     rubroSeleccionado= int(input("Seleccione el número del rubro para mostrar sus calificaciones: ")) - 1
     print(f"   ===Calificaciones del rubro '{list(rubros.keys())[rubroSeleccionado]}'===")
+    contador=1
     for tema, calif in rubros[list(rubros.keys())[rubroSeleccionado]].items():  #items (tema, calif) en diccionario [2D]
-        print(f"\t{tema}  -  {calif}")
+        print(f"{contador}.\t{tema}  -  {calif}")
+        contador+=1
+    return list(rubros.keys())[rubroSeleccionado]
+
+def eliminar_calificacion(rubros):
+    rubroSeleccionado=mostrar_calificaciones(rubros)
+    califEliminar=int(input("Ingrese el número de la asignación para eliminar su registro:")) - 1
+        #Seleccionar rubro a eliminar - acceso a diccionario por "indice"/lista
+    rubroEliminar=list(rubros[rubroSeleccionado].keys())   #Rubro-Diccionario Externo
+    asignacionEliminar=rubroEliminar[califEliminar]        #Asignación-Diccionario Interno
+    del rubros[rubroSeleccionado][asignacionEliminar]
+         
 
 
 
 
-rubros=registrar_rubros()
+
+
+rubros={    "Examen": {"Exam1":10, "Exam2":9}, 
+            "Tareas": {"Tarea1":8, "Tarea2":7, "MapaMental":9.6}, 
+            "Proyecto": {"Proyecto1":9, "Investigacion":10, "Infografia":8.5} }
+
 print("")
-registrar_calificacion(rubros)
+for key, value in rubros.items():
+    print(key, value)
 print("")
-print(rubros)
+eliminar_calificacion(rubros)
 print("")
-registrar_calificacion(rubros)
-print("")
-mostrar_calificaciones(rubros)
-print("")
-
-
-
+for key, value in rubros.items():
+    print(key, value)
 
 
 
